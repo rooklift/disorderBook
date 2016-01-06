f9df5f953b58b3b0d3af6ba53c7a032c7a19c51f
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2016-01-06 17:56:50 +0000

    cleanup bots and tests folders

--- tests/disorder_vs_real_test.py
+++ tests/disorder_vs_real_test.py
@@ -2,8 +2,8 @@
 # the quotes that result. One of these servers could be the official test server, though other
 # people might place orders on it at the same time.
 
-
-import copy, inspect, json, requests, time, random
+import random
+import stockfighter_minimal as sf
 
 
 API_URL_1 = "http://127.0.0.1:8000/ob/api/"
@@ -23,203 +23,24 @@ SYMBOL_2 = "FOOBAR"
 TEST_SIZE = 200
 SEED = 155178
 
-# ------------------------------------------------------------------------------------
-
-_API_URL = API_URL_1        # This is what's actually used by get_json_from_url()
-
-_api_cookie_text = "api_key={}".format(API_KEY_1)
-_extra_headers = {"X-Starfighter-Authorization" : API_KEY_1, "Cookie" : _api_cookie_text}
-
-def change_api_key(k):
-    global _api_cookie_text
-    global _extra_headers
-    
-    _api_cookie_text = "api_key={}".format(k)
-    _extra_headers = {"X-Starfighter-Authorization" : k, "Cookie" : _api_cookie_text}
-    
-def change_url(u):
-    global _API_URL
-    _API_URL = u
-
-
 
 random.seed(SEED)
+INFO = sf.Order()
 
-
-
-# --------------- necessary stuff that could be in its own file ------------------------------------------
-
-class Order():
-
-    attributes_and_types = {            # These are all the attributes that can ever be set
-        "account" : str,
-        "venue" : str,
-        "stock" : str,
-        "price" : int,
-        "qty" : int,
-        "direction" : str,
-        "orderType" : str
-    }
-    
-    synonyms = {
-        "symbol" : "stock"
-    }
-    
-    attributes = attributes_and_types.keys()
-
-    def __init__(self, att_dict=None):
-        for attribute in self.attributes:
-            setattr(self, attribute, None)
-
-        # Simplest just to do the above even when the caller has tried to provide values.
-        # So if the following fails in anyway we will still have None.
-        
-        if type(att_dict) is dict:
-            for key in att_dict.keys():
-                try:
-                    setattr(self, key, att_dict[key])
-                except:
-                    print("Couldn't set attribute {} to {}".format(key, att_dict[key]))
-        elif att_dict is None:
-            pass
-        else:
-            print("Ignoring non-dictionary argument.")
-    
-    # Our own __setattr__ forbids the addition of any other attributes,
-    # as well as ensuring the attribute has the correct type or None
-    
-    def __setattr__(self, name, val):
-        if name in self.synonyms:
-            name = self.synonyms[name]
-        if name not in self.attributes:
-            print("Not a valid attribute: '{}'.".format(name))
-            return
-        if val is not None:                        # None values do get saved unaltered
-            correct_type = self.attributes_and_types[name]
-            try:
-                val = correct_type(val)
-            except:
-                print("Unable to convert {} to type {}".format(val, correct_type))
-                return
-        super().__setattr__(name, val)
-    
-    def __getattr__(self, name):
-        if name in self.synonyms:
-            name = self.synonyms[name]
-        return super().__getattribute__(name)
-    
-    def as_json(self):
-        resultdict = dict()
-        for attribute in self.attributes:
-            val = getattr(self, attribute)
-            resultdict[attribute] = val
-        return json.dumps(resultdict)
-    
-    def dump(self):
-        for attribute in self.attributes:
-            val = getattr(self, attribute)
-            print("{} = {}".format(attribute, val if val is not None else ""))
-    
-    def set_to_buy(self):
-        self.direction = "buy"
-    
-    def set_to_sell(self):
-        self.direction = "sell"
-    
-    def set_to_limit(self):
-        self.orderType = "limit"
-    
-    def set_to_ioc(self):
-        self.orderType = "immediate-or-cancel"
-    
-    def set_to_fok(self):
-        self.orderType = "fill-or-kill"
-    
-    def set_to_market(self):
-        self.orderType = "market"
-    
-    def copy(self):
-        return copy.copy(self)
-
-
-
-def get_json_from_url(url, postdata = None, deletemethod = False, verbose = False, superverbose = False, require_ok = True):        # Note: POST overrides DELETE
-
-    caller = inspect.stack()[1][3]
-
-    try:
-        if postdata is not None:                # Might be "" which should be allowed, so test for not None
-            raw = requests.post(url, data = postdata, headers = _extra_headers)
-        elif deletemethod:
-            raw = requests.delete(url, headers = _extra_headers)
-        else:
-            raw = requests.get(url, headers = _extra_headers)
-    except TimeoutError:
-        print("get_json_from_url() called from {}() - TimeoutError".format(caller))
-        return None
-    except requests.exceptions.ConnectionError:
-        print("get_json_from_url() called from {}() - requests.exceptions.ConnectionError".format(caller))
-        return None
-    
-    # We got some sort of reply...
-    
-    try:
-        result = raw.json()
-    except ValueError:
-        print(raw.text)
-        print("RESULT WAS NOT VALID JSON.")
-        return None
-    
-    # The reply was valid JSON...
-    
-    if require_ok:
-        if "ok" not in result:
-            print(raw.text)
-            print("THE 'ok' FIELD WAS NOT PRESENT.")
-            return None
-        if result["ok"] != True:
-            print(raw.text)
-            print("THE 'ok' FIELD WAS NOT TRUE.")
-            return None
-    
-    # All tests have passed. Nothing has been printed (since we only print errors).
-    
-    if superverbose:
-        print(raw.headers)
-    if verbose:
-        print(raw.text)
-    
-    return result
-
-
-def execute(order, verbose = False):
-    return get_json_from_url(_API_URL + "venues/{}/stocks/{}/orders".format(order.venue, order.stock), postdata = order.as_json(), verbose = verbose)
-
-def quote(venue, symbol, verbose = False):
-    return get_json_from_url(_API_URL + "venues/{}/stocks/{}/quote".format(venue, symbol), verbose = verbose)
-
-def orderbook(venue, symbol, verbose = False):
-    return get_json_from_url(_API_URL + "venues/{}/stocks/{}".format(venue, symbol), verbose = verbose)
 
 def set_from_account_1(order):
     order.account = ACCOUNT_1
     order.venue = VENUE_1
     order.symbol = SYMBOL_1
-    change_api_key(API_KEY_1)
-    change_url(API_URL_1)
+    sf.change_api_key(API_KEY_1)
+    sf.set_web_url(API_URL_1)
 
 def set_from_account_2(order):
     order.account = ACCOUNT_2
     order.venue = VENUE_2
     order.symbol = SYMBOL_2
-    change_api_key(API_KEY_2)
-    change_url(API_URL_2)
-
-# ---------------------------------------------------------------------------------
-
-
-INFO = Order()
-
+    sf.change_api_key(API_KEY_2)
+    sf.set_web_url(API_URL_2)
 
 def clear_the_books():
     global INFO
@@ -230,16 +51,16 @@ def clear_the_books():
     INFO.price = 1
 
     set_from_account_1(INFO)
-    execute(INFO)
+    sf.execute(INFO)
     set_from_account_2(INFO)
-    execute(INFO)
+    sf.execute(INFO)
 
     INFO.direction = "sell"
 
     set_from_account_1(INFO)
-    execute(INFO)
+    sf.execute(INFO)
     set_from_account_2(INFO)
-    execute(INFO)
+    sf.execute(INFO)
 
     # Set the last price and size...
 
@@ -249,20 +70,21 @@ def clear_the_books():
     INFO.direction = "sell"
 
     set_from_account_1(INFO)
-    execute(INFO)
+    sf.execute(INFO)
     set_from_account_2(INFO)
-    execute(INFO)
+    sf.execute(INFO)
 
     INFO.direction = "buy"
 
     set_from_account_1(INFO)
-    execute(INFO)
+    sf.execute(INFO)
     set_from_account_2(INFO)
-    execute(INFO)
+    sf.execute(INFO)
 
 
 
 clear_the_books()
+
 
 for n in range(TEST_SIZE):
     INFO.price = random.randint(1, 5000)
@@ -271,24 +93,24 @@ for n in range(TEST_SIZE):
     INFO.orderType = random.choice(["limit", "limit", "limit", "limit", "market", "immediate-or-cancel", "fill-or-kill"])
     
     set_from_account_1(INFO)
-    res1 = execute(INFO)
+    res1 = sf.execute(INFO)
     id1 = res1["id"]
     if n == 0:
         first_id_1 = id1
     if n == TEST_SIZE - 1:
         last_id_1 = id1
-    q1 = quote(INFO.venue, INFO.symbol)
-    o1 = orderbook(INFO.venue, INFO.symbol)
+    q1 = sf.quote(INFO.venue, INFO.symbol)
+    o1 = sf.orderbook(INFO.venue, INFO.symbol)
         
     set_from_account_2(INFO)
-    res2 = execute(INFO)
+    res2 = sf.execute(INFO)
     id2 = res2["id"]
     if n == 0:
         first_id_2 = id2
     if n == TEST_SIZE - 1:
         last_id_2 = id2
-    q2 = quote(INFO.venue, INFO.symbol)
-    o2 = orderbook(INFO.venue, INFO.symbol)
+    q2 = sf.quote(INFO.venue, INFO.symbol)
+    o2 = sf.orderbook(INFO.venue, INFO.symbol)
     
     print("IDs (adjusted, should match): {}, {} ----- {} {} @ {} ({})".format(id1 - first_id_1, id2 - first_id_2, INFO.direction, INFO.qty, INFO.price, INFO.orderType))
     
@@ -342,15 +164,6 @@ for n in range(TEST_SIZE):
 print("TEST_SIZE =", TEST_SIZE)
 print("SEED =", SEED)
 print()
-
-set_from_account_1(INFO)
-quote(INFO.venue, INFO.symbol, verbose = True)
-print("last - first == {} (expected {})".format(last_id_1 - first_id_1, TEST_SIZE - 1))
-print()
-
-set_from_account_2(INFO)
-quote(INFO.venue, INFO.symbol, verbose = True)
-print("last - first == {} (expected {})".format(last_id_2 - first_id_2, TEST_SIZE - 1))
 
 input()
 
--- tests/simul_connections_test.py
+++ tests/simul_connections_test.py
@@ -1,177 +1,20 @@
-# OK this is not exactly small but the problem
-# won't be in the Order() class at least.
+import threading, time, queue, random
+import stockfighter_minimal as sf
 
+sf.set_web_url("http://127.0.0.1:8000/ob/api/")
+sf.change_api_key("exb123456")
 
-import copy, inspect, json, requests, threading, time, queue, random
-
-
-_API_URL = "http://127.0.0.1:8000/ob/api/"
-_API_KEY = "exb123456"
-
-_api_cookie_text = "api_key={}".format(_API_KEY)
-_extra_headers = {"X-Starfighter-Authorization" : _API_KEY, "Cookie" : _api_cookie_text}
+ACCOUNT = "EXB123456"
+VENUE = "TESTEX"
+SYMBOL = "FOOBAR"
 
 
 PRINT_LOCK = threading.Lock()
 
 
-class Order():
-
-	attributes_and_types = {			# These are all the attributes that can ever be set
-		"account" : str,
-		"venue" : str,
-		"symbol" : str,		# FIXME
-		"price" : int,
-		"qty" : int,
-		"direction" : str,
-		"orderType" : str
-	}
-	
-	synonyms = {
-		"stock" : "symbol"	# FIXME
-	}
-	
-	attributes = attributes_and_types.keys()
-
-	def __init__(self, att_dict=None):
-		for attribute in self.attributes:
-			setattr(self, attribute, None)
-
-		# Simplest just to do the above even when the caller has tried to provide values.
-		# So if the following fails in anyway we will still have None.
-		
-		if type(att_dict) is dict:
-			for key in att_dict.keys():
-				try:
-					setattr(self, key, att_dict[key])
-				except:
-					print("Couldn't set attribute {} to {}".format(key, att_dict[key]))
-		elif att_dict is None:
-			pass
-		else:
-			print("Ignoring non-dictionary argument.")
-	
-	# Our own __setattr__ forbids the addition of any other attributes,
-	# as well as ensuring the attribute has the correct type or None
-	
-	def __setattr__(self, name, val):
-		if name in self.synonyms:
-			name = self.synonyms[name]
-		if name not in self.attributes:
-			print("Not a valid attribute: '{}'.".format(name))
-			return
-		if val is not None:						# None values do get saved unaltered
-			correct_type = self.attributes_and_types[name]
-			try:
-				val = correct_type(val)
-			except:
-				print("Unable to convert {} to type {}".format(val, correct_type))
-				return
-		super().__setattr__(name, val)
-	
-	def __getattr__(self, name):
-		if name in self.synonyms:
-			name = self.synonyms[name]
-		return super().__getattribute__(name)
-	
-	def as_json(self):
-		resultdict = dict()
-		for attribute in self.attributes:
-			val = getattr(self, attribute)
-			resultdict[attribute] = val
-		return json.dumps(resultdict)
-	
-	def dump(self):
-		for attribute in self.attributes:
-			val = getattr(self, attribute)
-			print("{} = {}".format(attribute, val if val is not None else ""))
-	
-	def set_to_buy(self):
-		self.direction = "buy"
-	
-	def set_to_sell(self):
-		self.direction = "sell"
-	
-	def set_to_limit(self):
-		self.orderType = "limit"
-	
-	def set_to_ioc(self):
-		self.orderType = "immediate-or-cancel"
-	
-	def set_to_fok(self):
-		self.orderType = "fill-or-kill"
-	
-	def set_to_market(self):
-		self.orderType = "market"
-	
-	def copy(self):
-		return copy.copy(self)
-
-
-
-def get_json_from_url(url, postdata = None, deletemethod = False, verbose = False, superverbose = False, require_ok = True):		# Note: POST overrides DELETE
-
-	caller = inspect.stack()[1][3]
-
-	try:
-		if postdata is not None:				# Might be "" which should be allowed, so test for not None
-			raw = requests.post(url, data = postdata, headers = _extra_headers)
-		elif deletemethod:
-			raw = requests.delete(url, headers = _extra_headers)
-		else:
-			raw = requests.get(url, headers = _extra_headers)
-	except TimeoutError:
-		with PRINT_LOCK:
-			print("get_json_from_url() called from {}() - TimeoutError".format(caller))
-			return None
-	except requests.exceptions.ConnectionError:
-		with PRINT_LOCK:
-			print("get_json_from_url() called from {}() - requests.exceptions.ConnectionError".format(caller))
-			return None
-	
-	# We got some sort of reply...
-	
-	try:
-		result = raw.json()
-	except ValueError:
-		with PRINT_LOCK:
-			print(raw.text)
-			print("RESULT WAS NOT VALID JSON.")
-		return None
-	
-	# The reply was valid JSON...
-	
-	if require_ok:
-		if "ok" not in result:
-			with PRINT_LOCK:
-				print(raw.text)
-				print("THE 'ok' FIELD WAS NOT PRESENT.")
-			return None
-		if result["ok"] != True:
-			with PRINT_LOCK:
-				print(raw.text)
-				print("THE 'ok' FIELD WAS NOT TRUE.")
-			return None
-	
-	# All tests have passed. Nothing has been printed (since we only print errors).
-	
-	if superverbose:
-		with PRINT_LOCK:
-			print(raw.headers)
-	if verbose:
-		with PRINT_LOCK:
-			print(raw.text)
-	
-	return result
-
-
-def execute(order, verbose = False):
-	return get_json_from_url(_API_URL + "venues/{}/stocks/{}/orders".format(order.venue, order.stock), postdata = order.as_json(), verbose = verbose)
-
-
 def execute_from_queue(task_queue, result_queue, verbose = False):
 	order = task_queue.get()
-	result = execute(order, verbose = verbose)
+	result = sf.execute(order, verbose = verbose)
 	result_queue.put(result)
 	task_queue.task_done()
 	return
@@ -179,11 +22,11 @@ def execute_from_queue(task_queue, result_queue, verbose = False):
 
 def stress_test():
 
-	INFO = Order()
+	INFO = sf.Order()
 	
-	INFO.account = "EXB123456"
-	INFO.venue = "TESTEX"
-	INFO.symbol = "FOOBAR"
+	INFO.account = ACCOUNT
+	INFO.venue = VENUE
+	INFO.symbol = SYMBOL
 	INFO.orderType = "limit"
 	INFO.qty = 100
 	
